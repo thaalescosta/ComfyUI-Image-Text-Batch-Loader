e5595cfa0b748fb56d425b6b9ee3bc59399b28c7
Thales Costa <thaalescosta@gmail.com>
2026-03-28 14:35:21 -0300

    Rename auto_prompt_loader and auto_image_prompt_loader files to use 'text'

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from .auto_image_loader import NODE_CLASS_MAPPINGS as _img_cls, NODE_DISPLAY_NAME_MAPPINGS as _img_disp
-from .auto_prompt_loader import NODE_CLASS_MAPPINGS as _prompt_cls, NODE_DISPLAY_NAME_MAPPINGS as _prompt_disp
-from .auto_image_prompt_loader import NODE_CLASS_MAPPINGS as _imgprompt_cls, NODE_DISPLAY_NAME_MAPPINGS as _imgprompt_disp
+from .auto_text_loader import NODE_CLASS_MAPPINGS as _prompt_cls, NODE_DISPLAY_NAME_MAPPINGS as _prompt_disp
+from .auto_image_text_loader import NODE_CLASS_MAPPINGS as _imgprompt_cls, NODE_DISPLAY_NAME_MAPPINGS as _imgprompt_disp
 
 NODE_CLASS_MAPPINGS = {**_img_cls, **_prompt_cls, **_imgprompt_cls}
 NODE_DISPLAY_NAME_MAPPINGS = {**_img_disp, **_prompt_disp, **_imgprompt_disp}
